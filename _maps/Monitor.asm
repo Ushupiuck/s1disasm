d5399dda89be22e977bf11d1077ddf7ca79ef75a
Ushupiuck <giancarloknd@hotmail.com>
2021-03-29 01:48:12 -0300

    Port Sonic 2 level headers. (To-do: upgrade to s3k format to support dynamic art & palettes)

--- _maps/Monitor.asm
+++ _maps/Monitor.asm
@@ -2,46 +2,46 @@
 ; Sprite mappings - output from SonMapEd - Sonic 1 format
 ; --------------------------------------------------------------------------------
 
-SME_UhLM_:	
-		dc.w SME_UhLM__18-SME_UhLM_, SME_UhLM__1E-SME_UhLM_	
-		dc.w SME_UhLM__29-SME_UhLM_, SME_UhLM__34-SME_UhLM_	
-		dc.w SME_UhLM__3F-SME_UhLM_, SME_UhLM__4A-SME_UhLM_	
-		dc.w SME_UhLM__55-SME_UhLM_, SME_UhLM__60-SME_UhLM_	
-		dc.w SME_UhLM__6B-SME_UhLM_, SME_UhLM__76-SME_UhLM_	
-		dc.w SME_UhLM__81-SME_UhLM_, SME_UhLM__8C-SME_UhLM_	
-SME_UhLM__18:	dc.b 1	
+SME_SNfwm:	
+		dc.w SME_SNfwm_18-SME_SNfwm, SME_SNfwm_1E-SME_SNfwm	
+		dc.w SME_SNfwm_29-SME_SNfwm, SME_SNfwm_34-SME_SNfwm	
+		dc.w SME_SNfwm_3F-SME_SNfwm, SME_SNfwm_4A-SME_SNfwm	
+		dc.w SME_SNfwm_55-SME_SNfwm, SME_SNfwm_60-SME_SNfwm	
+		dc.w SME_SNfwm_6B-SME_SNfwm, SME_SNfwm_76-SME_SNfwm	
+		dc.w SME_SNfwm_81-SME_SNfwm, SME_SNfwm_8C-SME_SNfwm	
+SME_SNfwm_18:	dc.b 1	
 		dc.b $EF, $F, 0, 0, $F0	
-SME_UhLM__1E:	dc.b 2	
+SME_SNfwm_1E:	dc.b 2	
 		dc.b $F2, 5, 0, $10, $F8	
 		dc.b $EF, $F, 0, 0, $F0	
-SME_UhLM__29:	dc.b 2	
+SME_SNfwm_29:	dc.b 2	
 		dc.b $F2, 5, 8, $10, $F8	
 		dc.b $EF, $F, 0, 0, $F0	
-SME_UhLM__34:	dc.b 2	
+SME_SNfwm_34:	dc.b 2	
 		dc.b $F2, 5, 0, $14, $F8	
 		dc.b $EF, $F, 0, 0, $F0	
-SME_UhLM__3F:	dc.b 2	
+SME_SNfwm_3F:	dc.b 2	
 		dc.b $F2, 5, 0, $18, $F8	
 		dc.b $EF, $F, 0, 0, $F0	
-SME_UhLM__4A:	dc.b 2	
+SME_SNfwm_4A:	dc.b 2	
 		dc.b $F2, 5, 0, $20, $F8	
 		dc.b $EF, $F, 0, 0, $F0	
-SME_UhLM__55:	dc.b 2	
+SME_SNfwm_55:	dc.b 2	
 		dc.b $F2, 5, 0, $24, $F8	
 		dc.b $EF, $F, 0, 0, $F0	
-SME_UhLM__60:	dc.b 2	
+SME_SNfwm_60:	dc.b 2	
 		dc.b $F2, 5, 0, $28, $F8	
 		dc.b $EF, $F, 0, 0, $F0	
-SME_UhLM__6B:	dc.b 2	
+SME_SNfwm_6B:	dc.b 2	
 		dc.b $F2, 5, 0, $2C, $F8	
 		dc.b $EF, $F, 0, 0, $F0	
-SME_UhLM__76:	dc.b 2	
-		dc.b $F2, 5, 0, $1C, $F8	
-		dc.b $EF, $F, 0, 0, $F0	
-SME_UhLM__81:	dc.b 2	
+SME_SNfwm_76:	dc.b 2	
 		dc.b $F2, 5, 0, $30, $F8	
 		dc.b $EF, $F, 0, 0, $F0	
-SME_UhLM__8C:	dc.b 8	
+SME_SNfwm_81:	dc.b 2	
+		dc.b $F2, 5, 0, $1C, $F8	
+		dc.b $EF, $F, 0, 0, $F0	
+SME_SNfwm_8C:	dc.b 8	
 		dc.b $FF, 0, 0, $34, $F0	
 		dc.b 7, 0, 0, 3, $F0	
 		dc.b $FF, 0, 0, $35, $F8	
