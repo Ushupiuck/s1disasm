d3656af2d60b146893168798ddcf2be643f5f73e
Ushupiuck <giancarloknd@hotmail.com>
2023-10-15 22:41:15 -0300

    Update Special Stage Mappings & VRAM Pointers.asm

--- _inc/Special Stage Mappings & VRAM Pointers.asm
+++ _inc/Special Stage Mappings & VRAM Pointers.asm
@@ -57,13 +57,13 @@ specialStageData: macro frame,mappings,palette,vram
 		specialStageData	0, Map_SS_R,      0, ArtTile_SS_R_Block
 		specialStageData	1, Map_Bump,      0, ArtTile_SS_Bumper
 		specialStageData	2, Map_Bump,      0, ArtTile_SS_Bumper
-		specialStageData	0, Map_SS_R,      0, ArtTile_SS_Zone_1
-		specialStageData	0, Map_SS_R,      0, ArtTile_SS_Zone_2
-		specialStageData	0, Map_SS_R,      0, ArtTile_SS_Zone_3
-		specialStageData	0, Map_SS_R,      0, ArtTile_SS_Zone_4
-		specialStageData	0, Map_SS_R,      0, ArtTile_SS_Zone_5
-		specialStageData	0, Map_SS_R,      0, ArtTile_SS_Zone_6
-		specialStageData	0, Map_Ring,      1, ArtTile_Ring
+		specialStageData	1, Map_Ring,      1, ArtTile_Ring ; the following are unused
+		specialStageData	1, Map_Ring,      1, ArtTile_Ring
+		specialStageData	1, Map_Ring,      1, ArtTile_Ring
+		specialStageData	1, Map_Ring,      1, ArtTile_Ring
+		specialStageData	1, Map_Ring,      1, ArtTile_Ring
+		specialStageData	1, Map_Ring,      1, ArtTile_Ring
+		specialStageData	0, Map_Ring,      1, ArtTile_Ring ; Until here.
 		specialStageData	0, Map_SS_Chaos3, 0, ArtTile_SS_Emerald
 		specialStageData	0, Map_SS_Chaos3, 1, ArtTile_SS_Emerald
 		specialStageData	0, Map_SS_Chaos3, 2, ArtTile_SS_Emerald
