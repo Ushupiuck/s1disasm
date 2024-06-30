7a840ac1ad23bc5a962a436843ab55d6fe971580
Clownacy <clownacy@yahoo.com>
2024-06-30 02:15:10 +0100

    Merge pull request #87 from devon-artmeier/AS
    Fix remaining old names in sound driver when FixBugs is enabled

--- s1.sounddriver.asm
+++ s1.sounddriver.asm
@@ -1444,9 +1444,9 @@ InitMusicPlayback:
 		move.w	d5,SMPS_RAM.v_soundqueue0(a6)
 		move.b	d6,SMPS_RAM.v_soundqueue2(a6)
 		move.b	#$80,SMPS_RAM.v_sound_id(a6)	; set music to $80 (silence)
-		lea	SMPS_RAM.v_music_track_ram.TrackVoiceControl(a6),a1
+		lea	SMPS_RAM.v_music_dac_track.VoiceControl(a6),a1
 		lea	FMDACInitBytes(pc),a2
-		moveq	#SMPS_MUSIC_DAC_FM_TRACK_COUNT-1,d1	; 7 DAC/FM tracks
+		moveq	#SMPS_MUSIC_FM_DAC_TRACK_COUNT-1,d1	; 7 DAC/FM tracks
 		bsr.s	.writeloop
 		lea	PSGInitBytes(pc),a2
 		moveq	#SMPS_MUSIC_PSG_TRACK_COUNT-1,d1	; 3 PSG tracks
