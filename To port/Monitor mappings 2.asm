4a7ae865818459993a1b2e4039ac0afd9394ba4f
Ushupiuck <giancarloknd@hotmail.com>
2025-05-07 22:30:02 -0300

    To be updated?
    Can't remember, it's been a while

---
+++ To port/Monitor mappings 2.asm
@@ -0,0 +1,73 @@
+SME_O1Co9_0:	mappingsTable
+	mappingsTableEntry.w	SME_O1Co9_18
+	mappingsTableEntry.w	SME_O1Co9_1E
+	mappingsTableEntry.w	SME_O1Co9_29
+	mappingsTableEntry.w	SME_O1Co9_34
+	mappingsTableEntry.w	SME_O1Co9_3F
+	mappingsTableEntry.w	SME_O1Co9_4A
+	mappingsTableEntry.w	SME_O1Co9_55
+	mappingsTableEntry.w	SME_O1Co9_60
+	mappingsTableEntry.w	SME_O1Co9_6B
+	mappingsTableEntry.w	SME_O1Co9_76
+	mappingsTableEntry.w	SME_O1Co9_81
+	mappingsTableEntry.w	SME_O1Co9_8C
+
+SME_O1Co9_18:	spriteHeader
+	spritePiece	-$10, -$11, 4, 4, 0, 0, 0, 0, 0
+SME_O1Co9_18_End
+
+SME_O1Co9_1E:	spriteHeader
+	spritePiece	-8, -$B, 2, 2, $10, 0, 0, 0, 0
+	spritePiece	-$10, -$11, 4, 4, 0, 0, 0, 0, 0
+SME_O1Co9_1E_End
+
+SME_O1Co9_29:	spriteHeader
+	spritePiece	-8, -$B, 2, 2, $14, 0, 0, 0, 0
+	spritePiece	-$10, -$11, 4, 4, 0, 0, 0, 0, 0
+SME_O1Co9_29_End
+
+SME_O1Co9_34:	spriteHeader
+	spritePiece	-8, -$B, 2, 2, $18, 0, 0, 0, 0
+	spritePiece	-$10, -$11, 4, 4, 0, 0, 0, 0, 0
+SME_O1Co9_34_End
+
+SME_O1Co9_3F:	spriteHeader
+	spritePiece	-8, -$B, 2, 2, $154, 0, 0, 0, 0
+	spritePiece	-$10, -$11, 4, 4, 0, 0, 0, 0, 0
+SME_O1Co9_3F_End
+
+SME_O1Co9_4A:	spriteHeader
+	spritePiece	-8, -$B, 2, 2, $24, 0, 0, 0, 0
+	spritePiece	-$10, -$11, 4, 4, 0, 0, 0, 0, 0
+SME_O1Co9_4A_End
+
+SME_O1Co9_55:	spriteHeader
+	spritePiece	-8, -$B, 2, 2, $28, 0, 0, 0, 0
+	spritePiece	-$10, -$11, 4, 4, 0, 0, 0, 0, 0
+SME_O1Co9_55_End
+
+SME_O1Co9_60:	spriteHeader
+	spritePiece	-8, -$B, 2, 2, $2C, 0, 0, 0, 0
+	spritePiece	-$10, -$11, 4, 4, 0, 0, 0, 0, 0
+SME_O1Co9_60_End
+
+SME_O1Co9_6B:	spriteHeader
+	spritePiece	-8, -$B, 2, 2, $30, 0, 0, 1, 0
+	spritePiece	-$10, -$11, 4, 4, 0, 0, 0, 0, 0
+SME_O1Co9_6B_End
+
+SME_O1Co9_76:	spriteHeader
+	spritePiece	-8, -$B, 2, 2, $34, 0, 0, 0, 0
+	spritePiece	-$10, -$11, 4, 4, 0, 0, 0, 0, 0
+SME_O1Co9_76_End
+
+SME_O1Co9_81:	spriteHeader
+	spritePiece	-8, -$B, 2, 2, $20, 0, 0, 0, 0
+	spritePiece	-$10, -$11, 4, 4, 0, 0, 0, 0, 0
+SME_O1Co9_81_End
+
+SME_O1Co9_8C:	spriteHeader
+	spritePiece	-$10, -1, 4, 2, $38, 0, 0, 0, 0
+SME_O1Co9_8C_End
+
+	even
---
+++ To port/Monitor mappings.asm
@@ -0,0 +1,47 @@
+SME_O1Co9:
+SME_O1Co9_0: 	dc.w SME_O1Co9_18-SME_O1Co9
+SME_O1Co9_2: 	dc.w SME_O1Co9_1E-SME_O1Co9
+SME_O1Co9_4: 	dc.w SME_O1Co9_29-SME_O1Co9
+SME_O1Co9_6: 	dc.w SME_O1Co9_34-SME_O1Co9
+SME_O1Co9_8: 	dc.w SME_O1Co9_3F-SME_O1Co9
+SME_O1Co9_A: 	dc.w SME_O1Co9_4A-SME_O1Co9
+SME_O1Co9_C: 	dc.w SME_O1Co9_55-SME_O1Co9
+SME_O1Co9_E: 	dc.w SME_O1Co9_60-SME_O1Co9
+SME_O1Co9_10: 	dc.w SME_O1Co9_6B-SME_O1Co9
+SME_O1Co9_12: 	dc.w SME_O1Co9_76-SME_O1Co9
+SME_O1Co9_14: 	dc.w SME_O1Co9_81-SME_O1Co9
+SME_O1Co9_16: 	dc.w SME_O1Co9_87-SME_O1Co9
+SME_O1Co9_18: 	dc.b $1
+	dc.b $EF, $F, $0, $0, $F0
+SME_O1Co9_1E: 	dc.b $2
+	dc.b $F5, $5, $0, $10, $F8
+	dc.b $EF, $F, $0, $0, $F0
+SME_O1Co9_29: 	dc.b $2
+	dc.b $F5, $5, $0, $154, $F8
+	dc.b $EF, $F, $0, $0, $F0
+SME_O1Co9_34: 	dc.b $2
+	dc.b $F5, $5, $0, $14, $F8
+	dc.b $EF, $F, $0, $0, $F0
+SME_O1Co9_3F: 	dc.b $2
+	dc.b $F5, $5, $0, $18, $F8
+	dc.b $EF, $F, $0, $0, $F0
+SME_O1Co9_4A: 	dc.b $2
+	dc.b $F5, $5, $0, $1C, $F8
+	dc.b $EF, $F, $0, $0, $F0
+SME_O1Co9_55: 	dc.b $2
+	dc.b $F5, $5, $0, $20, $F8
+	dc.b $EF, $F, $0, $0, $F0
+SME_O1Co9_60: 	dc.b $2
+	dc.b $F5, $5, $0, $24, $F8
+	dc.b $EF, $F, $0, $0, $F0
+SME_O1Co9_6B: 	dc.b $2
+	dc.b $F5, $5, $0, $28, $F8
+	dc.b $EF, $F, $0, $0, $F0
+SME_O1Co9_76: 	dc.b $2
+	dc.b $F5, $5, $0, $2C, $F8
+	dc.b $EF, $F, $0, $0, $F0
+SME_O1Co9_81: 	dc.b $1
+	dc.b $EF, $F, $0, $0, $F0
+SME_O1Co9_87: 	dc.b $1
+	dc.b $FF, $D, $0, $30, $F0
+	even
